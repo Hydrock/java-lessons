c72569a18a32d5fa4e54ed9ee784ed0d6f67756b
Вечканов Алексей <avechkanov@alfabank.ru>
2023-07-15 15:59:50 +0300

    wip: third lesson

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -3,3 +3,15 @@
 
 rm -rf ./build
 rm ./lessons/lesson_2/Demo.jar ./lessons/lesson_2/Demo.class
+
+rm -rf ./lessons/lesson_3/settings.gradle
+rm -rf ./lessons/lesson_3/gradlew
+rm -rf ./lessons/lesson_3/gradlew.bat
+rm -rf ./lessons/lesson_3/build.gradle
+rm -rf ./lessons/lesson_3/.gitattributes
+rm -rf ./lessons/lesson_3/.gitignore
+rm -rf ./lessons/lesson_3/gradle
+rm -rf ./lessons/lesson_3/.gradle
+rm -rf ./lessons/lesson_3/build
+rm -rf ./lessons/lesson_3/bin
+
